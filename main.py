0774dfc4124c27646b551e88e219a840c89826c7
FrancoJulianB <frajuba11@gmail.com>
2022-07-23 12:10:19 -0500

    Agregadon comentarios y reorganizacion de codigo

--- main.py
+++ main.py
@@ -7,16 +7,22 @@ from webdriver_manager.chrome import ChromeDriverManager
 s=Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=s)
 
-driver.get('https://www.zonaprop.com.ar/departamentos-alquiler-nueva-cordoba-1-habitacion-2-ambientes.html')
+base_link = 'https://www.zonaprop.com.ar/departamentos-alquiler-nueva-cordoba-con-balcon-desde-1-hasta-2-habitaciones-2-ambientes-publicado-hace-menos-de-1-mes-mas-10000-pesos-orden-publicado-descendente.html'
 
+#Buscamos los elementos que contienen precio.
+driver.get(base_link)
 all_prices = driver.find_elements(By.CSS_SELECTOR, "div.components__Price-sc-12dh9kl-4.inzZeR")
-all_prices_text = []
 
+
+#Extraccion de precios en lista.
+all_prices_text = []
 for all_prices in all_prices:
     text = all_prices.text
     if text != 'Consultar precio' :
-        all_prices_text.append(int((re.findall('\d+', text)[0]) + (re.findall('\d+', text)[1]))) #Sumamos a la lista enteros, que se obtienen de sumar la primera parte de la lista re.findall con la segunda (Ej: '50' + '000' para hacer 50000)
+        #Sumamos a la lista enteros, que se obtienen de sumar la primera parte de la lista re.findall con la segunda 
+        #(Ej: '50' + '000' para hacer 50000)
+        all_prices_text.append(int((re.findall('\d+', text)[0]) + (re.findall('\d+', text)[1]))) 
         print(text)
 
 avg_price = round(sum(all_prices_text) / len(all_prices_text), 2)
-print('El precio promedio de alquiler es $' + str(avg_price))
+print('El precio promedio de alquiler es $' + str(avg_price)) 
